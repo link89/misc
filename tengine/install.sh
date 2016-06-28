566dfc8a4e3f23758b8a9fc73a5e4c17af792702
Weihong,Xu <xuweihong02@meituan.com>
2016-06-29 02:07:12 +0800

    tengine: configuration for php

--- tengine/install.sh
+++ tengine/install.sh
@@ -6,3 +6,27 @@ export LUAJIT_INC=/usr/local/include/luajit-2.0
 make clean; CC=clang; CFLAGS="-g -O0" ./configure --enable-mods-static=all --with-ld-opt="-Wl,-rpath,/usr/local/lib" --with-debug --with-http_upstream_check_module --with-http_v2_module --with-http_dyups_module --with-http_dyups_lua_api --with-http_sysguard_module && make
 
 ln -s /usr/local/nginx/sbin/nginx /usr/bin/nginx
+
+cat << EOF > /lib/systemd/system/nginx.service
+
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+ExecStartPre=/usr/bin/nginx -t
+ExecStart=/usr/bin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chown nobody -R /usr/local/nginx
+
+firewall-cmd --add-port=80/tcp --permanent
+firewall-cmd --reload
