bea69f6fc85e08fa5c6c7543fd1a6555839eafa0
Weihong Xu <xuweihong02@meituan.com>
2016-09-07 12:07:16 +0800

    maven: hello world project

---
+++ maven/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.example.helloworld;
+public class HelloWorld{
+    public String sayHello(){
+        return "Hello Maven";
+    }
+    public static void main (String[] args){
+        System.out.print(new HelloWorld().sayHello());
+    }
+}
